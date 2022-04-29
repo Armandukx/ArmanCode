19dd69c98b6eedc8801cdb02a56a2a7d077eed7f
Armandukx <104468428+Armandukx@users.noreply.github.com>
2022-04-30 01:13:50 +0200

    Updated ArmanCode.py
    Changed chromedriver stuff

--- ArmanCode.py
+++ ArmanCode.py
@@ -1,6 +1,6 @@
 #made by Armandukx
 #imports
-import os
+from ssl import Options
 import pystyle
 from pystyle import Colorate, Colors, Write
 import selenium
@@ -12,6 +12,31 @@ from selenium.webdriver.support import expected_conditions as EC
 import pytesseract
 import numpy as np
 import keyboard #imports the keyboard
+print('getting "chromedriver.exe" location')
+print("")
+import os
+ 
+# This is to get the directory that the program
+# is currently running in.
+dir_path = os.path.dirname(os.path.realpath(__file__))
+ 
+for root, dirs, files in os.walk(dir_path):
+    for file in files:
+ 
+        # change the extension from '.mp3' to
+        # the one of your choice.
+        if file.endswith('.exe'):
+            f = open("temp.txt","w+")
+            f.write (root+'/'+str(file))
+            f.close()
+            f=open("temp.txt", "r") #opens text
+if f.mode == 'r':
+    contents =f.read()
+    print(contents)
+f.close()
+print ("")
+print("license")
+print("")
 #LICENSE
 print("--Terms & Conditions--")
 print("By using our app you agreed with our terms and conditions.")
@@ -22,24 +47,26 @@ print("We have all the rights to change/modify our terms & conditions/privacy po
 print("If you don't agree with our terms & conditions, you should stop using our services..")
 print("")
 print(Colorate.Horizontal(Colors.purple_to_blue, f"Designer and Coder: Armandukx"))
-print("")
 #MADE BY ARMANDUKX
 #imports continue
 import colorama
 from colorama import Fore
 import time
-import pyfiglet
-from pyfiglet import figlet_format
 from tkinter import *
 import os
 import pyperclip
 import time
 #start
-print(Fore.LIGHTGREEN_EX + '') #Color for the text
-print(figlet_format("Armandukx", font = "speed"))
+print(Colorate.Horizontal(Colors.green_to_red, f"_______                                _________      ______         "))
+print(Colorate.Horizontal(Colors.green_to_red, f"___    |_____________ _________ _____________  /___  ____  /_____  __"))
+print(Colorate.Horizontal(Colors.green_to_red, f"__  /| |_  ___/_  __ `__ \  __ `/_  __ \  __  /_  / / /_  //_/_  |/_/"))
+print(Colorate.Horizontal(Colors.green_to_red, f"_  ___ |  /   _  / / / / / /_/ /_  / / / /_/ / / /_/ /_  ,<  __>  <  "))
+print(Colorate.Horizontal(Colors.green_to_red, f"/_/  |_/_/    /_/ /_/ /_/\__,_/ /_/ /_/\__,_/  \__,_/ /_/|_| /_/|_|  ")) # https://patorjk.com/software/taag/#p=display&f=Speed&t=Armandukx
+print("")
 print(Colorate.Horizontal(Colors.blue_to_red, f"Join our discord to be up to date! https://discord.gg/qrRZvdT73s"))
 print("")
-
+print(Colorate.Horizontal(Colors.red_to_purple, f"IMPORTANT: if you get some google error that means you're on a timer"))
+print("")
 print(Colorate.Horizontal(Colors.red_to_purple, f"[0] - Views ? > "))
 print(Colorate.Horizontal(Colors.red_to_purple, f"[1] - Share ? > "))
 Chosen = Write.Input("", Colors.red_to_purple, interval=0.0001)
@@ -49,7 +76,7 @@ pyperclip.copy(s1)
 s2 = pyperclip.paste()
 print(s2) #end of copying and pasting.
 if Chosen.lower().startswith("1"):
-    driver = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+    driver = webdriver.Chrome(contents)
     driver.get("https://zefoy.com/")
     url='https://zefoy.com/'
     time.sleep(10)
@@ -58,26 +85,26 @@ if Chosen.lower().startswith("1"):
     print(Colorate.Horizontal(Colors.red_to_purple, f"Sending Shares..."))
     driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
     element.click()
-    time.sleep(4)
+    time.sleep(2)
     element = driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[5]/div/button")
     driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
     element.click()
-    time.sleep(4)
+    time.sleep(2)
     element = driver.find_element_by_xpath("/html/body/div[4]/div[6]/div/form/div/input")
     driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
     element.click()
-    time.sleep(4)
+    time.sleep(2)
     s1 = (videourl) #copy's the video url
     pyperclip.copy(s1)
     s2 = pyperclip.paste()
     print(s2) #end of copying and pasting.
     keyboard.press_and_release('ctrl+v')
-    time.sleep(4)
+    time.sleep(2)
     #MADE BY ARMANDUKX
     element = driver.find_element_by_xpath("/html/body/div[4]/div[6]/div/form/div/div/button")
     driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
     element.click()
-    time.sleep(4)
+    time.sleep(2)
     element = driver.find_element_by_xpath("/html/body/div[4]/div[6]/div/div/div[1]/div/form/button")
     driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
     element.click()
@@ -86,9 +113,11 @@ if Chosen.lower().startswith("1"):
     driver.quit()
     print(Colorate.Horizontal(Colors.red_to_purple, f"Sent Shares..."))
     print(Colorate.Horizontal(Colors.red_to_purple, f"You must wait 5-8 minute's before trying again!"))
+    if os.path.exists("temp.txt"):
+          os.remove("temp.txt") #delets the .txt file
     time.sleep(20)
     quit()
-else: driver = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+else: driver = webdriver.Chrome(contents)
 driver.get("https://zefoy.com/")
 url='https://zefoy.com/'
 time.sleep(10)
@@ -97,26 +126,26 @@ driver.set_window_position(-10000,0)#1 SHARES
 print(Colorate.Horizontal(Colors.red_to_purple, f"Sending Views..."))
 driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
 element.click()
-time.sleep(4)
+time.sleep(2)
 element = driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[4]/div/button")
 driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
 element.click()
-time.sleep(4)
+time.sleep(2)
 element = driver.find_element_by_xpath("/html/body/div[4]/div[5]/div/form/div/input")
 driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
 element.click()
-time.sleep(4)
+time.sleep(2)
 s1 = (videourl) #copy's the video url
 pyperclip.copy(s1)
 s2 = pyperclip.paste()
 print(s2) #end of copying and pasting.
 keyboard.press_and_release('ctrl+v')
-time.sleep(4)
+time.sleep(2)
 #MADE BY ARMANDUKX
 element = driver.find_element_by_xpath("/html/body/div[4]/div[5]/div/form/div/div/button")
 driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
 element.click()
-time.sleep(4)
+time.sleep(2)
 element = driver.find_element_by_xpath("/html/body/div[4]/div[5]/div/div/div[1]/div/form/button")
 driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack','true');});",element)
 element.click()
@@ -125,5 +154,7 @@ time.sleep(10)
 driver.quit()
 print(Colorate.Horizontal(Colors.red_to_purple, f"Sent Views"))
 print(Colorate.Horizontal(Colors.red_to_purple, f"You must wait 5-8 minute's before trying again!"))
+if os.path.exists("temp.txt"):
+      os.remove("temp.txt") #delets the .txt file
 time.sleep(20)
 quit()
