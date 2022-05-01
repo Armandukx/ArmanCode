1533546115e17d5ec96d418b9e67e8497aacc9d0
Armandukx <104468428+Armandukx@users.noreply.github.com>
2022-05-01 13:33:03 +0200

    Added Setup.bat
    Automatically installs pip's

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+color 0A
+cls
+echo Downloading Modules...
+python -m pip install selenium
+python -m pip install pystyle
+python -m pip install numpy
+python -m pip install keyboard
+python -m pip install colorama
+python -m pip install pyperclip
+python -m pip install chromedriver-autoinstaller
+echo Done
+pause
